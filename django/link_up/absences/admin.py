f0da34d6a023d1eb7ccc46e666b270ae2f1d1e77
Malcovys <malcovys22.aps@gmail.com>
2023-07-15 16:36:35 +0300

    adjuste student tabele view

--- django/link_up/absences/admin.py
+++ django/link_up/absences/admin.py
@@ -8,10 +8,13 @@ from absences.models import TeacherModule
 from absences.models import Timetable
 from absences.models import Absence
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'matricule', 'group', 'number')
+
 admin.site.register(Teacher)
 admin.site.register(Module)
 admin.site.register(Group)
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(TeacherModule)
 admin.site.register(Absence)
 admin.site.register(Timetable)
